cd55c4546d14b194106775bdf2b85ab5294310b8
Yash Kavaiya <miteswati@gmail.com>
2020-07-06 19:51:59 +0530

    TimeConversion.java
    Week-2 Submission

---
+++ TimeConversion.java
@@ -0,0 +1,18 @@
+import java.util.*;
+
+public class TimeConversion
+{
+    public static void main(String[] args)
+    {
+        int days, minutes;
+        short years;
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter Minutes : ");
+        minutes = sc.nextInt();
+        days = (int)(minutes/(60*24)); // 1 day = 24*60 minutes
+        years = (short)(days/365); // 1 year = 365 day (normally) = 366 days (leap year)
+        days = days%365; // Remaining days after adding into years
+        days = days-(int)(years/4); // Counting for Leap years
+        System.out.println(years + " years " + days + " days");
+    }
+}
